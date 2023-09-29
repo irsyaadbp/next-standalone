913d0bd1c19caa9c840b3989b39f21b111be41d2
Irsyaad Budi <irsyaadbp01@gmail.com>
2023-09-29 22:23:32 +0700

    feat: initial project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverRuntimeConfig: {
+    ngihuy: process.env.NGIHUY,
+  },
+  output: "standalone",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
